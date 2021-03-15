f7679ed726e377f6922bb3a66e702c921c919224
Wellington Soares <wjuniorgm@gmail.com>
2021-03-14 22:46:47 -0300

    Second Bloc draft

--- Sources/CombineBLoC/CombineBLoC.swift
+++ Sources/CombineBLoC/CombineBLoC.swift
@@ -5,27 +5,48 @@
 //
 import Combine
 
-open class CombineBloc<Event, State> where Event: Equatable, State: Equatable {
-    public let bloc: Bloc<Event, State>
-    public init(state: State, mapEventToState: @escaping ((Event) -> (State)))
-    { bloc = Bloc(state: state, mapEventToState: mapEventToState) }
+final internal class BlocSubject<Event, State, Failure: Error>: Publisher {
+    typealias Output = State
+
+    init(initialValue: State, mapEventToState: @escaping (Event, State) -> State) {
+        self.subject = .init(initialValue)
+        self.mapEventToState = mapEventToState
+    }
+
+    func send(_ value: Event) {
+        subject.send(mapEventToState(value, subject.value))
+    }
+
+    func send(completion: Subscribers.Completion<Failure>) {
+        subject.send(completion: completion)
+    }
+
+    func send(subscription: Subscription) {
+        subject.send(subscription: subscription)
+    }
+
+    func receive<Downstream: Subscriber>(subscriber: Downstream) where Failure == Downstream.Failure, State == Downstream.Input {
+        subject.subscribe(subscriber)
+    }
+
+    private let subject: CurrentValueSubject<State, Failure>
+    private let mapEventToState: ((Event, State) -> State)
 }
 
-final public class Bloc<Event, State>: ObservableObject where Event: Equatable, State: Equatable {
-    final public func send(_ event: Event) {
-        let newState = self._mapEventToState(event)
-        if(self.state != newState) {
-            self.state = newState }
+class Bloc<Event, State>: ObservableObject {
+
+    @Published public internal(set) var state: State
+    private let subject: BlocSubject<Event, State, Never>
+    private var cancellable: AnyCancellable?
+
+    final func send(_ value: Event) {
+        subject.send(value)
     }
-    public internal(set) var state: State {
-        willSet {
-            objectWillChange.send()
-        }
+
+    init(initialValue: State, mapEventToState: @escaping (Event, State) -> State) {
+        self.state = initialValue
+        self.subject = BlocSubject<Event, State, Never> (initialValue: initialValue, mapEventToState: mapEventToState)
+        self.cancellable = self.subject.assign(to: \.state, on: self)
     }
-    internal init(state: State, mapEventToState: @escaping ((Event) -> (State))
-    ) {
-        self._mapEventToState = mapEventToState
-        self.state = state
-    }
-    private let _mapEventToState: (Event) -> (State)
+
 }
